99e87582ba14df68b81455001607f3c78166e2ec
David Konsumer <konsumer@jetboystudio.com>
2016-07-13 17:54:41 -0700

    even simpler build

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
-import express from 'express'
-const app = express()
-const port = process.env.PORT || 5000
+var express = require('express')
+var app = express()
+var port = process.env.PORT || 5000
 
 app.use(express.static('pub'))
 
